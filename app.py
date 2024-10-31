a64e8b19a52c38cd75b9d4fc1ea0f67ba3cc9b86
Mahidhar Reddy CH <mahidhar.reddy14@gmail.com>
2024-10-31 15:54:46 +0530

    added substract fun

---
+++ app.py
@@ -0,0 +1,12 @@
+
+def add(x,y):
+    """This is add function"""
+    return x+y
+
+def mul(x,y):
+    """This is multiply funtion"""
+    return x*y
+
+def minus(x,y):
+    """This is substract function"""
+    return x-y
